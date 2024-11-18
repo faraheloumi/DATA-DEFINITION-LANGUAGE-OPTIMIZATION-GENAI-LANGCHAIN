2fcd07b534376daffd40f0f91a668246b85bd621
faraheloumi <farah.elloumi2000@gmail.com>
2024-11-18 15:41:35 +0100

    fix: structure rectification

--- config/config.py
+++ config/config.py
@@ -1,4 +1,7 @@
 MODEL_ID = "meta-llama/Llama-3.2-3B"
 INSTRUCT_MODEL_ID = "meta-llama/Llama-3.2-3B-instruct"
-
+MODEL_NAME = "bigcode/starcoder2-3b"
 SQL_FILE_PATH = './data/usecase.sql'
+DDL_FILE_PATH = "./data/ddl.sql"
+SQL_CONTEXT_FILE_PATH = "./sql_analysis_results_sample.txt"
+OUTPUT_FILE = "./optimized_ddl.sql"
