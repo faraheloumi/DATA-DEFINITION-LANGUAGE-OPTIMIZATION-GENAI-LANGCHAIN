b5f3416eab43e63bd66a5c2d20de0cb38c83f950
faraheloumi <farah.elloumi2000@gmail.com>
2024-11-18 15:40:05 +0100

    fix: structure rectification

--- src/llm.py
+++ src/llm.py
@@ -1,3 +1,4 @@
+import torch
 from langchain_huggingface.llms import HuggingFacePipeline
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
@@ -10,3 +11,14 @@ def create_instruct_pipeline(instruct_model_id):
     tokenizer = AutoTokenizer.from_pretrained(instruct_model_id)
     model = AutoModelForCausalLM.from_pretrained(instruct_model_id)
     return HuggingFacePipeline(pipeline=pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=350, return_full_text=False))
+
+def create_llm_pipeline_starcoder(MODEL_NAME):
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    if tokenizer.pad_token_id is None:
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = AutoModelForCausalLM.from_pretrained(
+        MODEL_NAME, torch_dtype=torch.float16, device_map="auto" if device == "cuda" else None
+    )
+    return HuggingFacePipeline(pipeline=pipeline("text-generation", model=model, tokenizer=tokenizer, device=0 if torch.cuda.is_available() else -1, max_new_tokens=150, temperature=0.7, do_sample=True, return_full_text=False,))
+
